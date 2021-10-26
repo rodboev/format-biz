4e2a9e256cc62d7f8ba731b9bf79f89dba306dc0
Rod Boev <rod.boev@gmail.com>
2021-10-26 19:38:57 -0400

    Add main category field

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const createCsvStringifier = require("csv-writer").createObjectCsvStringifier;
 const fs = require("fs");
 const Transform = require("stream").Transform;
 
-// const counties = require('./counties.json');
+const area = "nyc";
+const state = "NY";
+
+const categories = require('./categories.json');
 
 const csvStringifier = createCsvStringifier({
   header: [
     { id: "name", title: "name" },
     { id: "display_phone", title: "phone" },
     { id: "categories", title: "categories" },
+    { id: "mainCategory", title: "mainCategory" },
     { id: "location.address1", title: "address1" },
     { id: "location.address2", title: "address2" },
     { id: "location.address3", title: "address3" },
@@ -22,11 +26,74 @@ const csvStringifier = createCsvStringifier({
   ],
 });
 
-let readStream = fs.createReadStream("businesses-nyc-filtered.csv");
-let writeStream = fs.createWriteStream("businesses-nyc-filtered-clean.csv");
+let readStream = fs.createReadStream(`businesses-${area}-filtered.csv`);
+let writeStream = fs.createWriteStream(`businesses-${area}-filtered-categorized.csv`);
 
 function removeNull(obj) {
   return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v !== "null"));
+}
+
+function findParent(cat) {
+  const parent = categories.find(o => o.alias === cat).parents;
+	if (parent.length === 0) return cat;
+	if (parent.length === 1) parent.toString();
+  // console.log(`${cat} --> ${parent}`);
+  return findParent(parent[0]);
+}
+
+function getWeights(cats) {
+  const weighted = ['restaurants', 'food', 'nightlife'];
+  let weights = [];
+  for (cat of cats) {
+    // cat = findName(cat);
+    const weight = weighted.includes(cat) ? 2 : 1;
+    const categoryExists = weights.find(o => o.category === cat);
+    if (categoryExists && categoryExists.weight) {
+      categoryExists.weight + weight;
+    }
+    else {
+      weights.push({
+        category: cat,
+        weight: weight
+      });
+    }
+  }
+  return weights;
+}
+
+function getParents(cats) {
+  const parents = [];
+  for (cat of cats) {
+    parents.push(findParent(cat));
+  }
+  return parents;
+}
+
+function findName(alias) {
+  return categories.find(o => o.alias === alias).title;
+}
+
+function getHeaviest(weights) {
+  const sorted = weights.sort(function(a, b) { return b.weight-a.weight });
+  const max = sorted[0].weight;
+  const atMax = sorted.filter(o => o.weight === max);
+  const last = atMax[atMax.length-1];
+  const first = atMax[0];
+  if (atMax.length > 1) {
+    const cats = atMax.map(o => o.category);
+    // console.log(`[WARNING] Equal weights (${max}): ${cats.join(', ')}`);
+    // console.log(`[WARNING] Using ${first.category}`);
+  }
+  return first.category;
+}
+
+function findAliases(str) {
+  const titles = str.split(', ');
+  const cats = [];
+  for (title of titles) {
+    cats.push(categories.find(o => o.title === title).alias);
+  }
+  return cats;
 }
 
 class CSVCleaner extends Transform {
@@ -35,13 +102,17 @@ class CSVCleaner extends Transform {
   }
 
   _transform(chunk, encoding, next) {
-    if (chunk["location.state"] === "NY" && chunk.display_phone && chunk["location.zip_code"] && chunk["location.zip_code"].length === 5 &&
+    if (chunk["location.state"] === state && chunk.display_phone && chunk["location.zip_code"] && chunk["location.zip_code"].length === 5 &&
         (chunk["location.zip_code"].startsWith("0") || chunk["location.zip_code"].startsWith("1"))
       ) {
       chunk = removeNull(chunk);
-      let categories = JSON.parse(chunk.categories).map(x => x.title).reverse().join(", ");
-      chunk.categories = categories;
-      // chunk.county = counties.find(x => x.zip === chunk["location.zip_code"])?.county;
+      const catsAll = JSON.parse(chunk.categories).map(x => x.title).reverse().join(", ");
+      const cats = JSON.parse(chunk.categories).map(o => o.alias).reverse();
+      const parents = getParents(cats);
+      const weights = getWeights(parents);
+      const heaviest = getHeaviest(weights);
+      chunk.categories = catsAll;
+      chunk.mainCategory = findName(heaviest);
       const chunkString = csvStringifier.stringifyRecords([chunk]);
       this.push(chunkString);
     }
